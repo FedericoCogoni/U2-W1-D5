e818780ec20f3eb4f72496ce466ff89d37738b9e
Federico Cogoni <federicocogoni90@gmail.com>
2024-03-29 21:55:14 +0100

    -aggiunta svg dinamico -aggiunti collegamenti ipertestuali -piccoli fix -MEDIA QUERY:FUNZIONANTE DA 760PX IN SU, VERSIONE MOBILE ANCORA DA REALIZZARE

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const header = document.querySelector("header")
-  const getStarted = document.querySelector("#get-started")
+  const getStarted = document.querySelector("#getStarted")
 
   window.addEventListener("scroll", function () {
     if (window.scrollY > 370) {
@@ -11,4 +11,20 @@ document.addEventListener("DOMContentLoaded", function () {
       getStarted.classList.remove("green-bg")
     }
   })
+})
+const allM = document.querySelectorAll("svg > g > g > g")
+function toggleM() {
+  allM.forEach(M => {
+    const randomTime = Math.round(Math.random() * 10000)
+    setTimeout(function () {
+      const toggleOpacity = Math.round(Math.random())
+      M.style.opacity = toggleOpacity
+    }, randomTime)
+  })
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  toggleM()
+
+  setInterval(toggleM, 10000)
 })
